431e1855681a6647a946330b27517ee52cc6e53d
EQUINOXY <ivkubishkin@gmail.com>
2020-11-28 12:59:41 +0300

    Initial commit

---
+++ src/com/company/first/Main.java
@@ -0,0 +1,12 @@
+package com.company.first;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int val = rand.nextInt(20);
+        Window w = new Window();
+        w.setVal(val);
+    }
+}
